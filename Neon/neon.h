d5e06736e328a055b824bcdc992bbe9216e178aa
HalavicH <halavich@gmail.com>
2021-02-21 17:34:12 +0200

    Neon: Add Blink macro
    Signed-off-by: HalavicH <halavich@gmail.com>

--- Neon/neon.h
+++ Neon/neon.h
@@ -1,6 +1,10 @@
 #ifndef NEON_H
 #define NEON_H
 
+#define BLINK(port, pin)                                                        \
+    HAL_GPIO_WritePin(port, pin, 1);                                            \
+    HAL_GPIO_WritePin(port, pin, 0);
+
 void neon_main();
 
 #endif /* NEON_H */
